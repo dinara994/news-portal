7fc8814d810dbddfb12687e5350985430f794725
dinara994 <di_94@mail.ru>
2021-09-16 19:52:26 +0600

    initial

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.css'
 import {NavLink} from "react-router-dom";
+import axios from "axios";
 
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false)
+    const [user, setUser] = useState({name: '', phone: '', email: ''})
+    const [sentSuccess, setSentSuccess] = useState(false)
+
+    const handleChange = (e) => {
+        setUser({...user, [e.target.id]: e.target.value})
+        //капируем все старое, название ключа записывается [клик  id] = выдатьт значение
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post('https://613f1faee9d92a0017e17474.mockapi.io/callback', user)
+            .then(() => setIsOpen(false))
+    }
+
+
     return (
         <header className="header ms-2 me-2 d-flex justify-content-between align-items-center">
             <NavLink  to='/' className="sides Logo">LOgo</NavLink>
@@ -13,9 +30,51 @@ const Header = () => {
                 <NavLink to='/modal'  className='me-3'>Application</NavLink>
                 <NavLink to='/signUp' className='me-3'>Sign up</NavLink>
                 <NavLink to='/signIn' className='me-3'>Sign in</NavLink>
+                <button className='close-modal' onClick={() => setIsOpen(true)}>Регистрация</button>
 
             </div>
+            <div className='d-flex justify-content-center align-content-center'>
+                {
+                    isOpen &&
+                    <div className='modal-window'>
+                        {/*//общий теневой div*/}
+                        <div className='modal-content'>
+                            {
+                               sentSuccess ? 'Успешно отправлено!' :
+                                <>
+                                    <h3>Заполните платформу</h3>
+                                    <form onSubmit={handleSubmit}>
+                                        <div className="mb-3">
+                                            <label htmlFor="name" className="form-label">Name</label>
+                                            <input onChange={handleChange} type="text" className="form-control" id="name" required/>
+                                        </div>
+                                        <div className="mb-3">
+                                            <label htmlFor="phone" className="form-label">Phone</label>
+                                            <input onChange={handleChange} type="phone" className="form-control" id="phone" required/>
+                                        </div>
+                                        <div className="mb-3">
+                                            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                                            <input onChange={handleChange} type="email" className="form-control" id="exampleInputEmail1"
+                                                   aria-describedby="emailHelp" required/>
+                                            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                                        </div>
+                                        <div className="mb-3 form-check">
+                                            <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                                            <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                                        </div>
+                                        <button type="submit" className="btn btn-primary" onClick={() => setIsOpen(true)}>Submit</button>
+                                    </form>
+                                </>
+                            }
+                            <button className='close-modal' onClick={() => setIsOpen(false)}>Х</button>
+                        </div>
+                    </div>
+                }
+
+            </div>
+
         </header>
     );
 };
+
 export default Header;
--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -3,58 +3,13 @@ import './style.css'
 import axios from "axios";
 
 const Modal = () => {
-    const [isOpen, setIsOpen] = useState(false)
-    const [callBack, setCallback] = useState({})
 
-const handleChange = (e) => {
-        setCallback({...callBack, [e.target.id] : e.target.value})
-    //капируем все старое, название ключа записывается [клик  id] = выдатьт значение
-}
-
-const handleSubmit = (e) => {
-        e.preventDefault()
-    axios.post('https://613f1faee9d92a0017e17474.mockapi.io/api/', callBack)
-        .then(({data}) => setIsOpen(false))
-}
     return (
         <>
-        <div className='d-flex justify-content-center align-content-center'>
-            <form onSubmit={handleSubmit}>
 
-                <div className="mb-3">
-                    <label htmlFor="name" className="form-label">Name</label>
-                    <input onChange={handleChange} type="text" className="form-control" id="name"  required/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="phone" className="form-label">Phone</label>
-                    <input onChange={handleChange} type="phone" className="form-control" id="phone" required/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input onChange={handleChange} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required/>
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-                </div>
-                <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                    <label className="form-check-label" htmlFor="exampleCheck1"  >Check me out</label>
-                </div>
-                <button type="submit" className="btn btn-primary"  onClick={() => setIsOpen(true)}>Submit</button>
-            </form>
-        </div>
-
-        {
-            isOpen &&
-            <div className='modal-window'>
-                {/*//общий теневой div*/}
-                <div className='modal-content'>
-                    <h3>Lorem ipsum dolor.</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, tenetur.</p>
-                    <button className='close-modal' onClick={() => setIsOpen(false)}>Х</button>
-                </div>
-            </div>
-        }
         </>
     );
 };
+
 
 export default Modal;
